8c6449d274dc23edcdb39395b09887757cae29ff
Mohammad Alshami <elshami312@gmail.com>
2023-09-21 10:13:19 +0300

    changes

---
+++ navigationexplore/src/App.tsx
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+//Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// screens
+import Details from './screens/Details';
+import Home from './screens/Home';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {productId: string};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Trending Products',
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            title: 'Product Details',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ navigationexplore/src/screens/Details.tsx
@@ -0,0 +1,42 @@
+import {StyleSheet, Text, View, Button} from 'react-native';
+import React from 'react';
+
+// navigation
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+const Details = ({route}: DetailsProps) => {
+  const {productId} = route.params;
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  return (
+    <View style={styles.container}>
+      <Text>Details: {productId}</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+        //onPress={() => navigation.goBack()}
+        //onPress={() => navigation.pop(2)}
+        //onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+};
+
+export default Details;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  smallText: {
+    color: '#000000',
+  },
+});
